08a9df8f48f9da3d78368b1013bc30a7efd8bbf0
Samuel <innovatests@gmail.com>
2016-10-27 14:24:44 -0700

    Add Google Maps with a single custom location

--- public/js/index.js
+++ public/js/index.js
@@ -16,3 +16,18 @@ function initializePage() {
 		e.preventDefault();
 	});
 }
+
+// Called by google maps' callback
+function initMap() {
+	// map.data.loadGeoJson('data.json');
+	console.log("InitMap called");
+	var uluru = {lat: -25.363, lng: 131.044};
+	var map = new google.maps.Map($('#map')[0], {
+	  zoom: 4,
+	  center: uluru
+	});
+	var marker = new google.maps.Marker({
+	  position: uluru,
+	  map: map
+	});
+}
