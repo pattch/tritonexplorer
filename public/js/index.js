23e30342b7e10fd17e43ac020d859e258457ff19
Samuel <innovatests@gmail.com>
2016-10-27 20:15:20 -0700

    Add Search bar with autocomplete to the google map, styling for the map.

--- public/js/index.js
+++ public/js/index.js
@@ -30,4 +30,8 @@ function initMap() {
 	  position: uluru,
 	  map: map
 	});
+
+	var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 }
