e1c5d3dbdecb30ed4e5da870e387142073efa935
Jiaming Bai <j2bai@ucsd.edu>
2016-11-02 13:50:42 -0700

    start on the login popup

---
+++ public/js/popup.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function openlogin(){
+  document.getElementById('loginform').style.display="block";
+}
+function hidelogin(){
+  document.getElementById('loginform').style.display="none";
+}
