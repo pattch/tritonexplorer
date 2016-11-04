33a7eea96001c069aac408b2fbe2b894d588d65e
Samuel <innovatests@gmail.com>
2016-11-03 20:17:28 -0700

    Missions populated from server.

--- routes/objective.js
+++ routes/objective.js
@@ -3,6 +3,8 @@
  * GET objective page.
  */
 
+var loaded_missions = require('../public/js/missions.json');
+
 exports.view = function(req, res){
-  res.render('objective');
+	res.render('objective', loaded_missions);
 };
