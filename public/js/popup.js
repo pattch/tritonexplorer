9bf8c226d67cad505833aaac9c8f010df455d5b7
Jiaming Bai <j2bai@ucsd.edu>
2016-11-09 13:44:49 -0800

    save data of login form

--- public/js/popup.js
+++ public/js/popup.js
@@ -106,3 +106,28 @@ function openHelp(){
 function hideHelp(){
   document.getElementById('helpForm').style.display="none";
 }
+
+//Handle login data from login form
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
+$(function() {
+    $('form').submit(function() {
+        $('#result').text(JSON.stringify($('loginform').serializeObject()));
+        return false;
+    });
+});
