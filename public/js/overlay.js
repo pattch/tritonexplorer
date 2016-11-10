f17efd55344f6a777ec281c3d877f5a8708bc776
Jiaming Bai <j2bai@ucsd.edu>
2016-11-10 13:48:08 -0800

    login has bug

--- public/js/overlay.js
+++ public/js/overlay.js
@@ -1,26 +1,34 @@
+
+var clicked;
+
 $(document).ready(function() {
-  console.log("wtf");
   initializeLoginOverlay();
 });
 
-var count = 0;
 function initializeLoginOverlay() {
+  if(clicked==1){
+    $('#overlay').addClass('hidden');
+    console.log(clicked);
+  }
+  console.log(clicked);
+  clicked = 1;
+
   console.log("Inside overlay");
 	var login_btn = $('#login');
 	var guest_btn = $('#guest');
 
-    if(count != 0) {
-        $('#overlay').addClass('hidden');
-    }
 
 	login_btn.click(function() {
+    clicked = 1;
+    console.log(clicked);
+
     $('#overlay').addClass('hidden');
     $('#loginform').removeClass('hidden');
 
 	});
 
 	guest_btn.click(function() {
+    clicked = 1;
     $('#overlay').addClass('hidden');
 	});
-
 }
