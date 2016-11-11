2a64d26471b2d07cf4e7cf4b38cb6bb195a06d90
Samuel <innovatests@gmail.com>
2016-11-10 16:55:15 -0800

    Fix Cookie setting for overlay

--- public/js/overlay.js
+++ public/js/overlay.js
@@ -1,17 +1,23 @@
-document.cookie = "clicked=0; max-age=31536000; path=/";
-var clicked = document.cookie;
-
+var clicked = 0;
 $(document).ready(function() {
   initializeLoginOverlay();
 });
 
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
 function initializeLoginOverlay() {
-  if(clicked==1){
+  clicked = getCookie("clicked");
+  account = getCookie("accountID");
+
+  if(clicked==1 || accountID != -1){
     $('#overlay').addClass('hidden');
     console.log(clicked);
   }
   console.log(clicked);
-  clicked = 1;
 
   console.log("Inside overlay");
 	var login_btn = $('#login');
@@ -24,11 +30,13 @@ function initializeLoginOverlay() {
 
     $('#overlay').addClass('hidden');
     $('#loginform').removeClass('hidden');
-
+    document.cookie = "clicked=1; max-age=" + cookie_time + "; path=/";
 	});
 
 	guest_btn.click(function() {
     clicked = 1;
     $('#overlay').addClass('hidden');
+    var cookie_time = 60 * 60 * 30; // 30 minutes
+    document.cookie = "clicked=1; max-age=" + cookie_time + "; path=/";
 	});
 }
